c902764cf1ba825bf137911b3e3cfc9a1e3671b8
abdulhakimsg <abdulhakimchi@gmail.com>
2018-10-17 09:26:04 +0800

    bugs fixed:css fixed

--- views/home.jsx
+++ views/home.jsx
@@ -11,16 +11,18 @@ class Home extends React.Component {
        <a className="btn btn-success" id="nearMe" href="location">Near me</a>
         <div>
         <h2 id="findStreet" >Find by Street name & Vehicle Type</h2>
+        <div class = "myForm">
         <form action="/result" method="get">
             <input type="text" name="name" placeholder="Type Street name" id="typeStreet"/>
             <input type="radio" name="type" value="C" className="circle" defaultChecked />
             <label htmlFor="car" className="radio">CAR(C)</label>
             <input type="radio" name="type" value="Y" className="circle" />
-            <label htmlFor="motor" className="radio">MOTORBIKE(Y)</label>
+            <label htmlFor="motor" className="radio">BIKE(Y)</label>
             <input type="radio" name="type" value="H" className="circle" />
             <label htmlFor="truck" className="radio">HEAVY(H)</label>
-            <input type="submit" value="Submit" className="btn btn-outline-primary" />
+            <input type="submit" value="Submit" className="btn btn-outline-primary" id="submitBtn" />
           </form> 
+          </div>
           </div>
           {/* <h2>See All</h2>
           <a href="/all"><button className="btn btn-outline-secondary">Show all</button> </a>  */}
